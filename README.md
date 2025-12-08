8b9063463734d2ad75ddfc401f2268360541b2a8
sf\adhvi <adhvi@fylkesmannen.no>
2025-12-08 10:56:31 +0100

    added readme

---
+++ README.md
@@ -0,0 +1,17 @@
+Repo for Advent of Code and other julekalendarar <3 
+
+Julekalendarar/
+│
+├── input/
+│   └── knowit/
+│       ├── knowit_Dag1_input.txt
+│       └── ...
+│
+├── Knowit/
+│   └── Knowit.cs       # Løysingsklasse for Knowit-oppgåver
+│
+├── Day1.cs             # (valfritt) Andre oppgåver du jobbar med
+├── Day2.cs             # (valfritt)
+│
+├── Program.cs          # Kallar valfri løysing/metode
+└── README.md
--- obj/Debug/net10.0/Julekalendarar.AssemblyInfo.cs
+++ obj/Debug/net10.0/Julekalendarar.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Julekalendarar")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab8008b27e42bd508cc5486c330162f4dd8f1833")]
 [assembly: System.Reflection.AssemblyProductAttribute("Julekalendarar")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Julekalendarar")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
