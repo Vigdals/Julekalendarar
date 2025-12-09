892257db9e5879f9a8a8c0929d8681cf4b578bc9
sf\adhvi <adhvi@fylkesmannen.no>
2025-12-09 08:06:22 +0100

    qweqwe

--- Program.cs
+++ Program.cs
@@ -1,11 +1,14 @@
 ﻿using Julekalendarar;
 using Julekalendarar.Knowit;
 
+// get base directory of the project
 var baseDir = AppContext.BaseDirectory;
 var projectDir = Directory.GetParent(baseDir).Parent.Parent.Parent.FullName;
-var filePath = Path.Combine(projectDir, "input\\input.txt");
 
-var lines = File.ReadAllLines(filePath);
+
+// --- Advent of Code ---
+var AdventOfCodeFilePath = Path.Combine(projectDir, "input\\input.txt");
+var lines = File.ReadAllLines(AdventOfCodeFilePath);
 
 //int answer = Day1.Solve(lines, debug: true);
 //Console.WriteLine($"Answer for Day 1: {answer}");
@@ -13,6 +16,7 @@ var lines = File.ReadAllLines(filePath);
 // var answer = Day1.SolvePartTwo(lines, debug: true);
 // Console.WriteLine($"Answer for Day 2: {answer}");
 
+// --- Knowit ---
 var inputKnowItFile = Path.Combine(projectDir, "input\\knowit\\knowit_Dag1_input.txt");
 var solver = new KnowitSolver(inputKnowItFile);
 string key = solver.SolveDay1();
